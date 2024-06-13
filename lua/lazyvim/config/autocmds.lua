c05510e5c2e4580d898a991e6229f8c4f803db26
laevatein29 <liyongchen29@gmail.com>
2024-06-13 15:35:19 +0800

    feat: update cmd

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -130,7 +130,33 @@ if true then
 end
 
 -- following function is a demo for building a vim cmd
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = {
+    "*.vue",
+    "*.js",
+    "*.ts",
+    "*.jsx",
+    "*.tsx",
+    "*.mjs",
+  },
+  command = ":EslintFixAll",
+})
 
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "vue",
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
 -- local function runEslintFixAll()
 --   local file_type = vim.bo.filetype
 --   local eslint_file_types = { "javascript", "typescript", "vue", "javascript.jsx", "typescript.tsx" }
